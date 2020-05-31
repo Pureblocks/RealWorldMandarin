2f45d4a9a02b04915f6b21845ed22e98aa080101
Thomas Meijers <thomasmeijers@live.nl>
2020-05-31 23:48:59 +0200

    Add user and user_characters tables

--- rwm-backend/SQL/01_create.sql
+++ rwm-backend/SQL/01_create.sql
@@ -7,3 +7,7 @@ CREATE TABLE hsk_words (id SERIAL PRIMARY KEY, level VARCHAR NOT NULL, simplifie
 CREATE TABLE characters (id INTEGER PRIMARY KEY, hanzi_character VARCHAR NOT NULL, keyword VARCHAR NOT NULL, primitive BOOLEAN NOT NULL);
 
 CREATE TABLE elements (id SERIAL PRIMARY KEY, character__id INTEGER REFERENCES characters(id) NOT NULL, order_position INTEGER NOT NULL, element__id INTEGER REFERENCES characters(id) NOT NULL);
+
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50) NOT NULL UNIQUE, email VARCHAR(50) NOT NULL UNIQUE, password_hash VARCHAR NOT NULL, salt VARCHAR NOT NULL);
+
+CREATE TABLE user_characters (id SERIAL PRIMARY KEY, user__id INTEGER NOT NULL REFERENCES users(id), character__id INTEGER REFERENCES characters(id) NOT NULL, story VARCHAR NOT NULL);
